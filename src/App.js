8ee4907799b2a5259d60f0d026d3d736a8215c89
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-03-11 05:35:02 +0200

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Layout from "./hoc/Layout/Layout";
+import { Route, Switch } from "react-router-dom";
+import SignUp from "./containers/Auth/SignUp/SignUp";
+import SignIn from "./containers/Auth/SignIn/SignIn";
+import SignOut from "./containers/Auth/SignOut/SignOut";
+import { connect } from "react-redux";
+import { Redirect } from "react-router";
+import * as actionCreators from "./store/actions/index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.checkAuthStatus();
+  }
+  onSignedInRoutes() {
+    return (
+      <Switch>
+        <Route path="/signout" exact component={SignOut} />
+        <Redirect exact from="/signin" to="/" />
+        <Route path="/" exact component={() => <p>hello</p>} />
+      </Switch>
+    );
+  }
+
+  onSignedOutRoutes() {
+    return (
+      <Switch>
+        <Route path="/signin" exact component={SignIn} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/" exact component={() => <p>hello</p>} />
+      </Switch>
+    );
+  }
+
+  routes() {
+    return this.props.isSignedIn
+      ? this.onSignedInRoutes()
+      : this.onSignedOutRoutes();
+  }
+
+  render() {
+    return <Layout isSignedIn={this.props.isSignedIn}>{this.routes()}</Layout>;
+  }
 }
+const mapStateToProps = state => {
+  return {
+    isSignedIn: state.signIn.token !== null
+  };
+};
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    checkAuthStatus: () => dispatch(actionCreators.checkAuthStatus())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
