60ab61b6159acfcbdfc27abd32643e168e82012e
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-03-15 02:26:35 +0200

    format file

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,37 +1,55 @@
-import React from 'react';
-import Classes from './Input.module.css';
-import MultiSelect from '@khanacademy/react-multi-select';
+import React from "react";
+import Classes from "./Input.module.css";
 
-const input = (props) => {
+const input = props => {
   let inputElement = null;
   switch (props.elementType) {
-    case ('input'):
-      inputElement = <input onChange={props.changed} className={Classes.InputElement}
-        {...props.elementConfig} value={props.value} />;
-      break;
-    case ('textarea'):
-      inputElement = <textarea onChange={props.changed} className={Classes.InputElement}
-        {...props.elementConfig} value={props.value} />;
-      break;
-    case ('select'):
+    case "input":
       inputElement = (
-        <select onChange={props.changed} className={Classes.InputElement}
-          value={props.value}>
-          {props.elementConfig.options.map((option) => {
-            return (<option key={option.value} value={option.value}>
-              {option.displayValue}
-            </option>
-            )
+        <input
+          onChange={props.changed}
+          className={Classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+      break;
+    case "textarea":
+      inputElement = (
+        <textarea
+          onChange={props.changed}
+          className={Classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          onChange={props.changed}
+          className={Classes.InputElement}
+          value={props.value}
+        >
+          {props.elementConfig.options.map(option => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.displayValue}
+              </option>
+            );
           })}
         </select>
       );
       break;
-    case ('multiselect'):
-      inputElement = <MultiSelect {...props.elementConfig}/>
-      break;
     default:
-      inputElement = <input onChange={props.changed} className={Classes.InputElement}
-        {...props.elementConfig} value={props.value} />;
+      inputElement = (
+        <input
+          onChange={props.changed}
+          className={Classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
   }
   return (
     <div>
@@ -39,6 +57,6 @@ const input = (props) => {
       {inputElement}
     </div>
   );
-}
+};
 
 export default input;
